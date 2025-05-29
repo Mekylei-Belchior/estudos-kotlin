c77becf057b6334af401290875d563c1324ee53a
Mekylei Belchior <mekylei@msn.com>
2025-05-29 19:14:39 -0300

    implementado lazy layout

--- jetpack-compose/delivery/app/src/main/java/com/mekylei/delivery/ui/components/ProductsSection.kt
+++ jetpack-compose/delivery/app/src/main/java/com/mekylei/delivery/ui/components/ProductsSection.kt
@@ -1,13 +1,12 @@
 package com.mekylei.delivery.ui.components
 
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -35,20 +34,14 @@ fun ProductsSection(
             fontSize = 20.sp,
             fontWeight = FontWeight(400)
         )
-        Row(
+        LazyRow(
             Modifier
-                .padding(
-                    top = 8.dp
-                )
-                .fillMaxWidth()
-                .horizontalScroll(rememberScrollState()),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
+                .padding(top = 8.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(16.dp)
         ) {
-            Spacer(Modifier)
-            for (p in products) {
-                ProductItem(product = p)
-            }
-            Spacer(Modifier)
+            items(products) { product -> ProductItem(product = product) }
         }
     }
 }
