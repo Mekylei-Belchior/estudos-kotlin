29cc3ed7c69e14e9e330bf35f2ecbf8d878ca92d
Mekylei Belchior <mekylei@msn.com>
2025-05-31 08:54:19 -0300

    implementado componente de busca

--- jetpack-compose/delivery/app/src/main/java/com/mekylei/delivery/ui/screens/HomeScreen.kt
+++ jetpack-compose/delivery/app/src/main/java/com/mekylei/delivery/ui/screens/HomeScreen.kt
@@ -1,11 +1,24 @@
 package com.mekylei.delivery.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -18,20 +31,41 @@ import com.mekylei.delivery.ui.theme.DeliveryTheme
 fun HomeScreen(
     sections: Map<String, List<Product>>
 ) {
-    LazyColumn(
-        Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(16.dp)
-    ) {
-        for (section in sections) {
-            val title = section.key
-            val products = section.value
-            item {
-                ProductsSection(
-                    title = title,
-                    products = products
+    var text by remember { mutableStateOf("") }
+
+    Column(Modifier.padding(top = 40.dp)) {
+        OutlinedTextField(
+            value = text,
+            onValueChange = { newValue -> text = newValue },
+            Modifier
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    bottom = 24.dp
                 )
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(100),
+            leadingIcon = {
+                Icon(Icons.Default.Search, contentDescription = null)
+            },
+            label = {Text(text = "O que você procura?")}
+        )
+
+        LazyColumn(
+            Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(8.dp)
+        ) {
+            for (section in sections) {
+                val title = section.key
+                val products = section.value
+                item {
+                    ProductsSection(
+                        title = title,
+                        products = products
+                    )
+                }
             }
         }
     }
