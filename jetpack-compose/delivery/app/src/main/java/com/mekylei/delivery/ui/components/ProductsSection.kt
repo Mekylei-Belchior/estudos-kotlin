5cfed65f0606e95c739cc09acc52ef9f3bfd1499
Mekylei Belchior <mekylei@msn.com>
2025-06-06 15:49:32 -0300

    criado componente Section

--- jetpack-compose/delivery/app/src/main/java/com/mekylei/delivery/ui/components/ProductsSection.kt
+++ jetpack-compose/delivery/app/src/main/java/com/mekylei/delivery/ui/components/ProductsSection.kt
@@ -26,24 +26,31 @@ fun ProductsSection(
     modifier: Modifier = Modifier
 ) {
     Column(modifier) {
-        Text(
-            text = title,
-            Modifier.padding(
-                start = 16.dp,
-                end = 16.dp
-            ),
-            fontSize = 20.sp,
-            fontWeight = FontWeight(400)
+        Section(
+            modifier = modifier,
+            title = {
+                Text(
+                    text = title,
+                    Modifier.padding(
+                        start = 16.dp,
+                        end = 16.dp
+                    ),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight(400)
+                )
+            },
+            content = {
+                LazyRow(
+                    Modifier
+                        .padding(top = 8.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    contentPadding = PaddingValues(16.dp)
+                ) {
+                    items(products) { product -> ProductItem(product = product) }
+                }
+            }
         )
-        LazyRow(
-            Modifier
-                .padding(top = 8.dp)
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            contentPadding = PaddingValues(16.dp)
-        ) {
-            items(products) { product -> ProductItem(product = product) }
-        }
     }
 }
 
---
+++ jetpack-compose/delivery/app/src/main/java/com/mekylei/delivery/ui/components/Section.kt
@@ -0,0 +1,22 @@
+package com.mekylei.delivery.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Section(
+    title: @Composable () -> Unit,
+    content: @Composable () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier) {
+        title()
+        Spacer(Modifier.height(16.dp))
+        content()
+    }
+}
